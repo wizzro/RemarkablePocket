cf0372940dc7e924c4d3b658747c373b2796bfd5
Thomas Montfort <thomas.montfort@renater.fr>
2025-02-17 12:15:05 +0100

    Use DelusionalLogic/rmapi fork for rmapi

--- update_rmapi_binaries.sh
+++ update_rmapi_binaries.sh
@@ -2,10 +2,10 @@
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-git clone git@github.com:ddvk/rmapi.git
+git clone git@github.com:DelusionalLogic/rmapi.git
 cd rmapi || exit
 git fetch
-git checkout "$(git tag --sort=-creatordate | head -n 1)"
+#git checkout "$(git tag --sort=-creatordate | head -n 1)"
 
 move() {
   mv rmapi "${SCRIPT_DIR}"/src/main/jib/usr/local/bin/rmapi_"${1}"
@@ -16,4 +16,4 @@ GOOS=linux GOARCH=arm GOARM=5 go build && move "arm"
 GOOS=linux GOARCH=amd64 go build && move "amd64"
 
 cd - || exit
-rm -rf rmapi
+#rm -rf rmapi
